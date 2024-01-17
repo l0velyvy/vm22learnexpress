7461dd9f1737d26fead0cc1b4a460034e696fc12
Evelyn Eichenberg <evelyn.eichenberg@tptlive.ee>
2024-01-17 15:15:50 +0200

    loplik update

--- server.js
+++ server.js
@@ -3,9 +3,14 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send(__dirname + '/Index.html');
+  res.sendFile(__dirname + '/Index.html');
   console.log(`Somebody visited`);
 });
+
+app.get('/page2', (req, res) => {
+    res.sendFile(__dirname + '/page2.html');
+    console.log(`Somebody visited`);
+  });
 
 app.listen(port, () => {
   console.log(`Example app listening on port http://localhost:${port}`);
