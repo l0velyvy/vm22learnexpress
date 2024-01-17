972b82def4621c70960246c8e6130b160cea0cd9
Evelyn Eichenberg <evelyn.eichenberg@tptlive.ee>
2024-01-17 15:44:27 +0200

    added nunjucks

--- server.js
+++ server.js
@@ -1,14 +1,20 @@
 const express = require('express');
+const nunjucks = require(`nunjucks`)
 const app = express();
 const port = 3000;
 
+nunjucks.configure('views', {
+    autoescape: true,
+    express: app
+});
+
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/Index.html');
+  res.render('Index.html');
   console.log(`Somebody visited`);
 });
 
 app.get('/page2', (req, res) => {
-    res.sendFile(__dirname + '/page2.html');
+    res.render('page2.html');
     console.log(`Somebody visited`);
   });
 
