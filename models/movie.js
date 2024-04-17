8eb14d1c8508d25b3c2116fdd93c19851ce3bfcf
Evelyn Eichenberg <evelyn.eichenberg@tptlive.ee>
2024-04-17 13:42:46 +0300

    .

---
+++ models/movie.js
@@ -0,0 +1,34 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Movie extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Movie.init({
+    name: DataTypes.STRING,
+    year: DataTypes.INTEGER,
+    description: DataTypes.TEXT,
+    name: {
+      type: DataTypes.STRING
+    },
+    year: {
+      type: DataTypes.INTEGER
+    },
+    description: {
+      type: DataTypes.TEXT
+    },
+  }, {
+    sequelize,
+    modelName: 'Movie',
+  });
+  return Movie;
+};
